39248b982e3bbc6a76256b0344dd2640eed3ac5c
Tarax-ikuro <ariasjonathan13015@gmail.com>
2022-09-29 12:17:03 +0200

    first

--- Site vitrine/script.js
+++ Site vitrine/script.js
@@ -1,20 +1,44 @@
-var navbar = document.querySelector(".navbar")
-var ham = document.querySelector(".ham")
+// Creation d'un objet qui va definir l'ensemble des options 
 
-// 
-function toggleHamburger(){
-  navbar.classList.toggle("showNav")
-  ham.classList.toggle("showClose")
+// root est l'élement racine qui permet de détecter la zone d'affichage
+// rootMargin depassement de marge pour qui'il y ait une visibilité
+
+// threshold detecte à partir de quel moment l'intersection estr détécté soit 1 donc l'enti
+
+// creation d'une constante ratio à 10 %
+const ratio = .1
+const options = {
+    root:null,
+rootMargin: '0px',
+thresold: ratio
+
+}
+// Creation de l'observer
+// Creation de la constante observer
+// Prend en parametre la  fonction uqi sera executer callback pendant le laps visible ou masqué 
+
+
+// Creation de la fonction
+// Comparaison de l'intersection ratio et le ratio 
+// Si elemnt visible on ne peut plus observer les choses 
+// Entries qui sont les differtns elements observés 
+const handleIntersect = function(entries, observer){
+entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+       entry.target.classList.add('reveal-visible')
+        observer.unobserve(entry.target)
+    }
+
+})
 }
 
-ham.addEventListener("click", toggleHamburger)
 
-// toggle when clicking on links
+const observer = IntersectionObserver(handleIntersect, options)
 
-// Creation d'une variable
-var menuLinks = document.querySelectorAll(".menuLink")
-menuLinks.forEach( 
-  function(menuLink) { 
-    menuLink.addEventListener("click", toggleHamburger) 
-  }
-)
+// Detection collision et zone d'affichage 
+// Observation de l'élément qui aura comme classe reveal
+// Detection de plusieurs element avec query selector all  + boucle sur l'ensemble des éléments 
+
+document.querySelectorAll('.reveal').forEach(function (r){
+    observer.observe(r)
+})
